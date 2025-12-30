1d8f1da08727084397b661b1e1ee4445acbbb99b
Igor da Silva Dias <rogigor1712@gmail.com>
2025-12-30 14:34:21 -0300

    Adicionando estrutura base do template Java

---
+++ src/main/java/com/igor/Main.java
@@ -0,0 +1,8 @@
+package com.igor;
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello and welcome!");
+        System.out.println("That's a template for Java projects");
+    }
+}
